dc574f4500d75c318c9e03ecb2c93185bf8fd733
bigggge <me@haoduoyu.cc>
2017-01-10 15:11:00 +0800

    Initial Binding

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Binding
 
->双向绑定的实现方法：
+### 技术基础
 
-常见的数据绑定的实现有脏值检测（Angular.js），
+实现数据绑定：
 
-基于 ES5 的 getter和setter 进行数据劫持（Vue.js），
+- 普通绑定 [实例](example/ancient-binding.html)
+- 常见的数据绑定的实现有脏值检测（Angular.js）[实例](example/dirty-check-binding.html)
+- 基于 ES5 的 getter和setter 进行数据劫持（Vue.js）[实例](example/hijack-binding.html)
 
-以及 ES 已被废弃的 Object.observe，和 ES6 中添加的 Proxy
+### 实现思路
+
+- 实现一个 Compiler 对元素的每个节点进行指令的扫描和提取；
+- 实现一个 Parser 去解析元素上的指令，把指令的意图通过 Updater 更新到 DOM 上
+- 实现一个 Watcher 能将 Parser 中每条指令的刷新函数和对应 Model 的字段联系起来；
+- 实现一个 Observer 对对象的的变化进行监测，发生变化时可以触发通知回调；
+
+### 模块划分
+
+![binding](binding.svg)
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
+var uglify = require('gulp-uglify');
+
+gulp.task('build', function () {
+    var bundler = watchify(browserify('./src/Binding.js', {debug: true}).transform(babel));
+
+    bundler.bundle()
+        .on('error', function (err) {
+            console.error(err);
+            this.emit('end');
+        })
+        .pipe(source('build.js'))
+        .pipe(buffer())
+        // .pipe(uglify())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.js', ['build']);
+})
+
+gulp.task('default', ['build', 'watch']);
